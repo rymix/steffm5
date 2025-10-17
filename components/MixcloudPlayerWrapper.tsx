73e945af49713a3ea9bb4c0721d9f5224a19d2d0
Steve Arnott <steve.a@smyle.co.uk>
2025-10-17 21:41:39 +0100

    Installed Next.js

---
+++ components/MixcloudPlayerWrapper.tsx
@@ -0,0 +1,22 @@
+import dynamic from 'next/dynamic';
+import React from 'react';
+
+// Dynamically import the MixcloudPlayer with no SSR since it uses browser APIs
+const MixcloudPlayer = dynamic(
+  () => import('../src/lib/MixcloudPlayer').then(mod => ({ default: mod.MixcloudPlayer })),
+  { 
+    ssr: false,
+    loading: () => <div>Loading player...</div>
+  }
+);
+
+interface MixcloudPlayerWrapperProps {
+  keys: string[];
+  autoPlay?: boolean;
+}
+
+const MixcloudPlayerWrapper: React.FC<MixcloudPlayerWrapperProps> = ({ keys, autoPlay = true }) => {
+  return <MixcloudPlayer keys={keys} autoPlay={autoPlay} />;
+};
+
+export default MixcloudPlayerWrapper;
---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Head from 'next/head';
+import MixcloudPlayerWrapper from '../components/MixcloudPlayerWrapper';
+
+const HomePage: React.FC = () => {
+  const sampleKeys = [
+    '/rymixxx/adventures-in-decent-music-volume-1/',
+    '/rymixxx/adventures-in-decent-music-volume-2/',
+    '/rymixxx/stefs-disco-mix/',
+    '/rymixxx/my-pair-of-shoes-volume-9-deep-n-soulful/',
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>Stef.FM - Funky House and Soul Music</title>
+        <meta name="description" content="Adventures in Decent Music - Funky House and Soul Music" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="container">
+        <h1>Stef.FM - Adventures in Decent Music</h1>
+        
+        <div className="demo-info">
+          <p>
+            <strong>Welcome to Stef.FM!</strong> Funky House and Soul Music powered by Next.js and React.
+          </p>
+        </div>
+
+        <MixcloudPlayerWrapper 
+          keys={sampleKeys}
+          autoPlay={true} 
+        />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
